f7a3094c32762626187698746b1109c1805a6d30
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-09-14 03:59:15 +0600

    JSON DB added and product updated

--- src/pages/Home/components/FeatureProduct.js
+++ src/pages/Home/components/FeatureProduct.js
@@ -1,16 +1,29 @@
+import { useEffect, useState } from "react";
 import { ProductCard } from "../../../components/Elements/ProductCard";
 
+
 export const FeatureProduct = () => {
+    const [products, setProducts]= useState([]);
+    useEffect(()=>{
+        async function fetchProducts(){
+            const response= await fetch("http://localhost:8000/featured_products");
+            const data= await response.json();
+            setProducts(data);
+            console.log(data);
+        }
+        fetchProducts();
+    },[]);
+
     return (
-        <section className="my-20">
-            <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">
-                Featured eBooks
-            </h1>
-            <div className="flex flex-wrap justify-center lg:flex-row">
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-            </div>
-        </section>
-    );
-};
+    <section className="my-20">
+        <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">Featured eBooks</h1>    
+        <div className="flex flex-wrap justify-center lg:flex-row">
+
+          { products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          )) }
+
+        </div>
+    </section>
+  )
+}
