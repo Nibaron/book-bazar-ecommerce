605f226afc04e69d17591a047eff1612189c4c47
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-09-17 08:03:38 +0600

    cart Context and Reducer functionality

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { ScrollToTop } from './components';
-import { FilterProvider } from './context';
+import { ScrollToTop } from "./components";
+import { FilterProvider, CartProvider } from "./context";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <FilterProvider>
-      <ScrollToTop/>
-      <ToastContainer closeButton={false} autoClose={2000}/>
-      <App />
-      </FilterProvider>
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <CartProvider>
+                <FilterProvider>
+                    <ScrollToTop />
+                    <ToastContainer closeButton={false} autoClose={2000} />
+                    <App />
+                </FilterProvider>
+            </CartProvider>
+        </Router>
+    </React.StrictMode>
 );
