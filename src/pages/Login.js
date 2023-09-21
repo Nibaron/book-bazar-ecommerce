7b4d808a69f94438bc77e983581fde6112508c97
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-09-21 13:11:47 +0600

    "login as guest" added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,6 +23,20 @@ export const Login = () => {
         data.accessToken ? navigate("/products"):toast.error(data);
 
     }
+    async function handleLoginGuest(){
+        email.current.value = "nibaron@example.com";
+        password.current.value = "12345678";
+        try{
+          const authDetail = {
+            email: email.current.value,
+            password: password.current.value
+          }
+          const data = await login(authDetail);
+          data.accessToken ? navigate("/products") : toast.error(data);
+        } catch(error){
+          toast.error(error.message, {closeButton: false, position: "top-center"});
+        }
+      }
 
     return (
         <main>
@@ -71,6 +85,7 @@ export const Login = () => {
                     Login
                 </button>
             </form>
+            <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
             <p className="mt-4 text-center text-black dark:text-white">
                 Don't have any account?{" "}
                 <Link
